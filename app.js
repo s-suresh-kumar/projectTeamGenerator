ac3be1442920595440597c73d77729d6d9a62011
Suresh SK <suresh.uncbc.coding@gmail.com>
2020-07-20 10:49:20 -0400

    working version -1

--- app.js
+++ app.js
@@ -9,10 +9,84 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { listenerCount } = require("process");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+const employees = [];
+const mgrAddlDetail = "office number";
+const engrAddlDetail = "github username";
+const internAddlDetail = "school name";
+let hireMoreMembers_g = "no";
+hireTeamMember();
+async function hireTeamMember() {
+  try {
+    const { name } = await inquirer.prompt({
+      message: "Name of Team Member :",
+      name: "name",
+    });
+    const { id } = await inquirer.prompt({
+      message: "Id of Team Member :",
+      name: "id",
+    });
+    const { email } = await inquirer.prompt({
+      message: "Email of Team Member :",
+      name: "email",
+    });
+    const { role } = await inquirer.prompt({
+      type: "list",
+      message: "Role of Team Member :",
+      choices: ["Engineer", "Intern", "Manager"],
+      name: "role",
+    });
 
+    let addlDetailMsg = "";
+    if (role === "Engineer") {
+      addlDetailMsg = "GitHub username";
+    } else if (role === "Intern") {
+      addlDetailMsg = "school name";
+    } else {
+      addlDetailMsg = "office phone number";
+    }
+    const { addlDetail } = await inquirer.prompt({
+      message: `Enter team member's ${addlDetailMsg}`,
+      name: "addlDetail",
+    });
+
+    const { hireMoreMembers } = await inquirer.prompt({
+      type: "list",
+      message: "Hire more Members:",
+      choices: ["yes", "no"],
+      name: "hireMoreMembers",
+    });
+    hireMoreMembers_g = hireMoreMembers;
+    console.log("hiremoremembers", hireMoreMembers);
+    switch (role) {
+      case "Manager":
+        employees.push(new Manager(name, id, email, addlDetail));
+        break;
+      case "Engineer":
+        employees.push(new Engineer(name, id, email, addlDetail));
+        break;
+      case "Intern":
+        employees.push(new Intern(name, id, email, addlDetail));
+        break;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+  console.log("hiremoremembers", hireMoreMembers_g);
+  if (hireMoreMembers_g == "yes") {
+    hireTeamMember();
+  } else {
+    console.log("team:", employees.toString());
+    const html = render(employees);
+    fs.writeFileSync(outputPath, html, "utf8");
+    return;
+  }
+}
+
+/*
 inquirer
   .prompt([
     {
@@ -98,7 +172,7 @@ inquirer
         break;
     }
   });
-
+*/
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
